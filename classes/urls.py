f8e2d2ef1dbe98ff178e83ce427952c5201b9f49
Jewoo Ryu <jewoo20000624@gmail.com>
2023-03-01 14:49:57 +0900

    강의 추천과 강의 수강 기능의 URL 수정

--- classes/urls.py
+++ classes/urls.py
@@ -6,6 +6,6 @@ router = routers.SimpleRouter()
 router.register('classes', ClassViewSet)
 
 urlpatterns = router.urls + [
-    path('recommend/<int:pk>/', RecommendView.as_view()),
-    path('join/<int:pk>/', JoinView.as_view())
+    path('classes/<int:pk>/recommend/', RecommendView.as_view()),
+    path('classes/<int:pk>/join/', JoinView.as_view())
 ]
