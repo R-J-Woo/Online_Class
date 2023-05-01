0e6c8496a3b35b98482cd24e184b33a06b1078c0
Jewoo Ryu <jewoo20000624@gmail.com>
2023-05-01 23:01:08 +0900

    로그아웃 테스트 코드 작성 완료

--- users/tests.py
+++ users/tests.py
@@ -22,21 +22,15 @@ class UserRegisterTest(APITestCase):
         # 회원가입 진행 후의 상태 코드가 201인지 확인
         self.assertEqual(response.status_code, 201)
 
-    # 로그인 테스트 할 때 회원가입과 마찬가지로 user_data를 설정했는데 안되는 이유는
-    # 모든 테스트 메소드를 실행할 때마다 장고에서는 테스트 DB를 초기화하기 때문이다
-    # 그래서 회원가입에 입력했던 user_data를 로그인할 때 그대로 사용하더라도 에러가 발생한다 -> SetUp 메소드 필요
 
-    # def test_login(self):
-    #     url = reverse('login_view')
-    #     user_data = {
-    #         "username": "testusername",
-    #         "password": "testpassword",
-    #     }
-    #     response = self.client.post(url, user_data)
-    #     # 로그인 진행 후의 상태 코드가 200인지 확인
-    #     self.assertEqual(response.status_code, 200)
+# 로그인 테스트 할 때 회원가입과 마찬가지로 user_data를 설정했는데 안되는 이유는
+# 모든 테스트 메소드를 실행할 때마다 장고에서는 테스트 DB를 초기화하기 때문이다
+# 그래서 회원가입에 입력했던 user_data를 로그인할 때 그대로 사용하더라도 에러가 발생한다 -> SetUp 메소드 필요
 
 
+# 기존 강의처럼 setUp 메소드로 유저를 생성한 후, 생성한 유저의 데이터로 로그인을 진행하여 상태코드가 200인지 확인하려고 하였으나
+# Token matching query does not exist 라는 에러가 발생했다
+# 그래서 상태코드를 비교하는 것이 아닌, 생성한 유저의 토큰 값을 가져온 후, 그것을 로그인 진행한 뒤 받은 토큰 값과 비교해서 테스트를 진행한다
 class LoginUserTest(APITestCase):
     def setUp(self):
         self.data = {"username": "testuser", "password": "testpassword"}
@@ -48,3 +42,10 @@ class LoginUserTest(APITestCase):
         url = reverse('login_view')
         response = self.client.post(url, self.data)
         self.assertEqual(response.data["token"], self.token.key)
+
+
+class LogoutUserTest(APITestCase):
+    def test_logout(self):
+        url = reverse('logout_view')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 202)
--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from .views import RegisterView, LoginView, LogoutView, ProfileView
 urlpatterns = [
     path('sign-up/', RegisterView.as_view(), name='register_view'),
     path('login/', LoginView.as_view(), name='login_view'),
-    path('logout/', LogoutView.as_view()),
+    path('logout/', LogoutView.as_view(), name='logout_view'),
     path('profile/<int:pk>/', ProfileView.as_view()),
 ]
