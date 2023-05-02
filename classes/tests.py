0f3592deb75b857ad625f961cd3bd236ad5d185a
Jewoo Ryu <jewoo20000624@gmail.com>
2023-05-02 22:58:58 +0900

    로그인 안했을 때 수업 생성을 시도하는 테스트 코드 작성 로그인을 안했을 때는 수업을 생성할 수 없어야 함

--- classes/tests.py
+++ classes/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 # Create your tests here.
+
+
+class ClassCreateTest(APITestCase):
+    def setUp(self):
+        self.user_data = {"username": "testuser", "password": "testpassword"}
+        self.class_data = {"title": "test class title", "category": "programming",
+                           "content": "test class content", "thumbnail": ""}
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword", email="test@gmail.com")
+        self.token = Token.objects.create(user=self.user)
+
+    def test_create_class_not_login(self):
+        url = '/classes/'
+        response = self.client.post(url, self.class_data)
+        self.assertEqual(response.status_code, 401)
