59000a2d6e9d7fec5bbc676f0e4bdb5e66ae9fb9
Jewoo Ryu <jewoo20000624@gmail.com>
2023-05-07 15:28:45 +0900

    로그인한 상태 / 로그인 안한 상태 구분해서 수업 삭제 테스트 코드 작성

--- classes/tests.py
+++ classes/tests.py
@@ -3,11 +3,15 @@ from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+from .models import Class
+from users.models import Profile
 
 # Create your tests here.
 
+# 수업 생성 테스트
 
-class ClassTest(APITestCase):
+
+class ClassCreateTest(APITestCase):
     def setUp(self):
         self.user_data = {"username": "testuser", "password": "testpassword"}
         self.class_data = {"title": "test class title", "category": "programming",
@@ -28,3 +32,31 @@ class ClassTest(APITestCase):
         response = self.client.post(
             url, self.class_data, HTTP_AUTHORIZATION="Token " + str(self.token))
         self.assertEqual(response.status_code, 201)
+
+
+# 수업 삭제 테스트
+class ClassDeleteTest(APITestCase):
+    def setUp(self):
+        self.user_data = {"username": "testuser", "password": "testpassword"}
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword", email="test@gmail.com")
+        self.token = Token.objects.create(user=self.user)
+        self.profile = Profile
+        self.cls = Class.objects.create(instructor=self.user, profile=Profile.objects.get(
+            user=self.user), title="test title", category="test category", content="test content", thumbnail="")
+
+    # 로그인을 안한 상태
+    def test_delete_class_not_login(self):
+        pk = self.cls.id
+        url = '/classes/' + str(pk) + '/'
+        print(url)
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 401)
+
+    # 로그인을 한 상태
+    def test_delete_class_login(self):
+        pk = self.cls.id
+        url = '/classes/' + str(pk) + '/'
+        response = self.client.delete(
+            url, HTTP_AUTHORIZATION="Token " + str(self.token))
+        self.assertEqual(response.status_code, 204)
